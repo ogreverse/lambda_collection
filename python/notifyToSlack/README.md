dd424d433ad9dfe88c44383d129ea2f2abadb05f
ogreverse <glado.o.guild@gmail.com>
2021-06-14 23:16:22 +0900

    [add] Slackへの通知スクリプト追加

---
+++ python/notifyToSlack/README.md
@@ -0,0 +1,9 @@
+# README
+
+## 参考サイト
+
+[Qiita記事](https://qiita.com/yhidetoshi/items/b717a95e0986a41784bc)
+
+## Slack Web Hook URL 取得先
+
+[https://api.slack.com/apps](https://api.slack.com/apps)
---
+++ python/notifyToSlack/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.8
+import urllib.request
+import urllib3
+import json
+import os
+
+def lambda_handler(event, context):
+    msg = event['Records'][0]['Sns']['Message']
+    set_fileds = [{
+        "title": "Aws Notification",
+        "value": msg,
+        "short": False
+    }]
+
+    data = {
+        'attachments':  [{
+            'color': 'danger',
+            'fields': set_fileds
+        }]
+    }
+
+    url = os.environ['HOOK_URL']
+    method = 'POST'
+    request_headers = { 'Content-Type': 'application/json; charset=utf-8' }
+    body = json.dumps(data).encode("utf-8")
+    request = urllib.request.Request(
+        url=url,
+        data=body,
+        method=method,
+        headers=request_headers
+    )
+
+    urllib.request.urlopen(request)
